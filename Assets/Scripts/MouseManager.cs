a658a2e746ef2618fe8a2660e9b15c78c9d79468
TheSoloLeveling <62574566+TheSoloLeveling@users.noreply.github.com>
2020-04-29 23:06:26 +0000

    Setting the different pointers of the mouse depending of the types of the object

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public LayerMask clickableLayer; //
+
+    public Texture2D pointer; // normal mouse
+    public Texture2D target; // target mouse
+    public Texture2D doorway; // dourway mouse
+
+    public EventVector3 OnClickEnvironment; // event to which we can connect the NavMeshAgent
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+                door = true;
+            }
+
+            else
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickEnvironment.Invoke(hit.point);  // the hitpoint from the raycast 
+            }
+        }
+
+        else
+        {
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
+        }
+
     }
 }
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }  // creating a custom class to send Vector3 informations through an event
